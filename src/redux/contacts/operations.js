56240f47d62d7d0c00caa4e8fb5917ed0fe39552
PlatinumKellerman <platinumm@ukr.net>
2022-08-26 01:44:59 +0300

    final final fix xD

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -4,38 +4,41 @@ import { toast } from 'react-toastify';
 
 export const getContactsAsync = createAsyncThunk(
   'contacts/getContacts',
-  async _ => {
+  async (_, { rejectWithValue }) => {
     try {
       const data = await contactsAPI.getContacts();
       return data;
     } catch (error) {
       toast.error(error);
+      return rejectWithValue(error);
     }
   }
 );
 
 export const addContactAsync = createAsyncThunk(
   'contacts/addContact',
-  async contact => {
+  async (contact, { rejectWithValue }) => {
     try {
       await contactsAPI.addContact(contact);
       toast.success(`Contact added successfully`);
       return await contactsAPI.getContacts();
     } catch (error) {
       toast.error(error);
+      return rejectWithValue(error);
     }
   }
 );
 
 export const deleteContactAsync = createAsyncThunk(
   'contacts/deleteContact',
-  async contactId => {
+  async (contactId, { rejectWithValue }) => {
     try {
       await contactsAPI.deleteContact(contactId);
       toast.success(`Contact deleted successfully`);
       return await contactsAPI.getContacts();
     } catch (error) {
       toast.error(error);
+      return rejectWithValue(error);
     }
   }
 );
