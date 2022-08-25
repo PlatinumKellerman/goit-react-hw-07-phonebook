b8aa55bbf7e53166b94a064d8e7c366d4ea89894
PlatinumKellerman <platinumm@ukr.net>
2022-08-25 20:57:54 +0300

    fix

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,8 +5,7 @@ export const getContacts = createAsyncThunk(
   'contacts/getContacts',
   async (_, { rejectWithValue }) => {
     try {
-      const data = await contactsAPI.getContacts();
-      return data;
+      return await contactsAPI.getContacts();
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -17,8 +16,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, { rejectWithValue }) => {
     try {
-      const data = await contactsAPI.addContact(contact);
-      return data;
+      return await contactsAPI.addContact(contact);
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -29,8 +27,8 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, { rejectWithValue }) => {
     try {
-      const data = await contactsAPI.deleteContact(contactId);
-      return data.id;
+      await contactsAPI.deleteContact(contactId);
+      return await contactsAPI.getContacts();
     } catch (error) {
       return rejectWithValue(error.message);
     }
